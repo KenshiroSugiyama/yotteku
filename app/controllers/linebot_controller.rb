a5288844266f9cb8d2f9d9489fe12de3335db210
hokudai <ken4600223@gmail.com>
2020-06-14 20:31:37 +0900

    linebot テンプレート　”予約する”

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -15,18 +15,93 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+          if event.message['text'].eql?('予約する')
+            client.reply_message(event['replyToken'], template)
+          end
         end
       end
-      client.reply_message(event['replyToken'], message)
     end
     head :ok
   end
 
 private
+
+def template
+    {
+      "type": "template",
+      "altText": "？",
+      "template": {
+          "type": "carousel",
+          "text": "何食べたい？",
+          "columns": [
+          {
+            "thumbnailImageUrl": "https://rimage.gnst.jp/rest/img/6sp1wtu60000/s_0n6m.jpg?t=1585036850"
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "肉系",
+            "text": "description",
+            "defaultAction": {
+                "type": "postback",
+                "label": "選択",
+                "data": "category=meat",
+                "text": "肉系"
+            },
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "選択",
+                    "data": "category=meat",
+                    "text": "肉系"
+                },
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdJUj8MRgLDP64ptvYCflvOwcOKBrjnvtWd3MX86QD1gNl8-el&usqp=CAU",
+            "imageBackgroundColor": "#000000",
+            "title": "魚介系",
+            "text": "description",
+            "defaultAction": {
+                "type": "postback",
+                "label": "選択",
+                "data": "category=fish",
+                "text": "魚介系"
+            },
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "選択",
+                    "data": "category=fish",
+                    "text": "魚介系"
+                },
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://tblg.k-img.com/resize/660x370c/restaurant/images/Rvw/131355/131355648.jpg?token=0890a11&api=v2"
+            "imageBackgroundColor": "#000000",
+            "title": "イタリアン",
+            "text": "description",
+            "defaultAction": {
+                "type": "postback",
+                "label": "選択",
+                "data": "category=italy",
+                "text": "イタリアン"
+            },
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "決定",
+                    "data": "category=italy",
+                    "text": "イタリアン"
+                },
+            ]
+          }
+      ],
+      "imageAspectRatio": "rectangle",
+      "imageSize": "cover"
+  }
+          ]
+      }
+    }
+  end
 
 # LINE Developers登録完了後に作成される環境変数の認証
   def client
