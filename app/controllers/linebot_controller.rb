e78d86d7cf389c55f46def96af53993386effa1f
hokudai <ken4600223@gmail.com>
2020-06-20 04:16:51 +0900

    category

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -47,16 +47,16 @@ class LinebotController < ApplicationController
             req = Request.find_by(user_id: user.id)
             req.update(number_of_people: e.to_i)
             client.reply_message(event['replyToken'], template3)
-          elsif e.eql?('今すぐ')||e.eql?('30分後')||e.eql?('１時間後')
+          elsif e.eql?('今すぐ')||　e.eql?('30分後')　||　e.eql?('１時間後')
             req = Request.find_by(user_id: user.id)
             req.update(time: e)
             client.reply_message(event['replyToken'], template5)
 
           elsif e.eql?('なし')||e.include?('要望')
             req = Request.find_by(user_id: user.id)
-            f = e.delete!('要望')
+            f = e.delete!('要望/n')
             req.update(hope: f)
-            category = Category.find(req.category_id)
+            @category = Category.find(req.category_id)
             client.reply_message(event['replyToken'], template4)
           end
         end
@@ -237,7 +237,7 @@ def template
       "altText": "this is a confirm template",
       "template": {
           "type": "confirm",
-          "text": "ありがとうございます。\r\nリクエストが完成しました\r\nジャンル： #{category.name} \r\n予算：　#{req.budget}\r\n人数： #{req.number_of_people.to_s}\r\n開始時間： #{req.time}\r\n要望:  #{req.hope}",
+          "text": "ありがとうございます。\r\nリクエストが完成しました\r\nジャンル： #{@category.name} \r\n予算：　#{req.budget}\r\n人数： #{req.number_of_people.to_s}\r\n開始時間： #{req.time}\r\n要望:  #{req.hope}",
           "actions": [
               {
                 "type": "message",
