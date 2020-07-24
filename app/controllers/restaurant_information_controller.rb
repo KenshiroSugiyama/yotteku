ce6a43fca4f6bf5bb96b315c9082b7223e120b59
hokudai <ken4600223@gmail.com>
2020-07-24 13:52:00 +0900

    restaurantInformation#create save後にpush通知送る設定なくす

--- app/controllers/restaurant_information_controller.rb
+++ app/controllers/restaurant_information_controller.rb
@@ -21,28 +21,7 @@ class RestaurantInformationController < ApplicationController
             Restaurant.find(current_restaurant.id).update(name: name,phone_number: phone_number,category_id: category_id)
             redirect_to restaurant_information_path(id: @info.id)
             flash[:notice] = '登録しました!Lineの画面にお戻り下さい。'
-            #line push 送信
-            uri = URI.parse("https://api.line.me/v2/bot/message/push")
-            headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + ENV['LINE_CHANNEL_TOKEN'],
-            }
-            user = @req.user_id
-            userId = User.find(user).uid
-            puts userId
-            post = {
-                'to': userId,
-                'messages': [
-                    {
-                        "type": "text",
-                        "text": "要望入力が完了しました！\r\nジャンル： #{@category.name} \r\n予算： #{@req.budget}\r\n人数： #{@req.number_of_people.to_s}\r\n開始時間： #{@req.time}\r\n要望:  #{@req.hope}"
-                    }
-                ]
-            }
-            puts userId
-            req = Net::HTTP.new(uri.host, uri.port)
-            req.use_ssl = uri.scheme === "https"
-            req.post(uri.path, post.to_json,headers)
+            
         else
             redirect_to action: "new"
             flash[:alert] = '登録に失敗しました'
