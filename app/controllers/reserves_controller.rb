8c9af894d32aedf0c326c72737e383c135896bb5
hokudai <ken4600223@gmail.com>
2020-08-14 00:50:43 +0900

    修正

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -2,8 +2,6 @@ class ReservesController < ApplicationController
   def index
     scout_ids = Request.where(res_id: params[:id]).pluck(:scout_id)
     @scout = Scout.find(scout_ids)
-    req = Request.find(@scout.request_id)
-    @user = User.find(req.user_id)
   end
   
   def show
